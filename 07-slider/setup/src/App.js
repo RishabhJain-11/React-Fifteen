e3e06acdaa3ad8262d69b1ba762e8bcb1d60c43e
Rishabh Jain <jainrishabh71011@gmail.com>
2022-10-24 10:22:09 +0530

    ..

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -5,9 +5,8 @@ import data from './data';
 
 
 function App() {
-
   const [people, setPeople] = useState(data);
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = React.useState(0);
 
   useEffect(() => {
     const lastIndex = people.length - 1;
@@ -23,10 +22,12 @@ function App() {
   useEffect(() => {
     let slider = setInterval(() => {
       setIndex(index + 1);
-    }, 3000);
+    }, 5000);
 
-    return () => clearInterval(slider);
-  }, index);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
 
   return (
     <section className="section">
@@ -45,11 +46,11 @@ function App() {
             position = 'activeSlide';
           }
           if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
-            position = 'lastIndex';
+            position = 'lastSlide';
           }
 
           return (
-            <article key={id}>
+            <article key={id} className={position}>
               <img src={image} alt={name} className="person-img" />
 
               <h4>{name}</h4>
