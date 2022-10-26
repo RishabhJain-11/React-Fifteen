12e73f3bdcf388afac1d68d9f0ad1db781b9b5f7
Rishabh Jain <jainrishabh71011@gmail.com>
2022-10-26 17:35:26 +0530

    ..

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
-import { useGlobalContext } from './context'
+import React from 'react';
+import phoneImg from './images/phone.svg';
+import { useGlobalContext } from './context';
 
 const Hero = () => {
   const { closeSubmenu } = useGlobalContext();
   return (
-    <section className='hero'>
-      <div className="hero-center">
-
+    <section className='hero' onMouseOver={closeSubmenu}>
+      <div className='hero-center'>
         <article className='hero-info'>
           <h1>
             Payments infrastructure <br />
@@ -18,15 +17,14 @@ const Hero = () => {
             Stripe’s software and APIs to accept payments, send payouts, and
             manage their businesses online.
           </p>
-          <button className='btn'>Start Now</button>
+          <button className='btn'>Start now</button>
         </article>
-
         <article className='hero-images'>
-          <img src={phoneImg} alt='phone-img' className='phone-img' />
+          <img src={phoneImg} className='phone-img' alt='phone' />
         </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- 13-stripe-submenus/setup/src/Sidebar.js
+++ 13-stripe-submenus/setup/src/Sidebar.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
 import { useGlobalContext } from './context'
-
+import sublinks from './data'
 const Sidebar = () => {
-  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
   return (
-    <div className={`${isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
-      }`}>
-      <aside className="sidebar">
+    <div
+      className={`${isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+        }`}
+    >
+      <aside className='sidebar'>
         <button className='close-btn' onClick={closeSidebar}>
           <FaTimes />
         </button>
