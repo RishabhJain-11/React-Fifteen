cf378c6582e29b66cb93233f6df7036a63824c34
Rishabh Jain <jainrishabh71011@gmail.com>
2022-10-29 13:26:01 +0530

    ..

--- 15-cocktails/setup/src/components/Navbar.js
+++ 15-cocktails/setup/src/components/Navbar.js
@@ -2,12 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../logo.svg'
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link>
+          <img src={logo} alt="cocktail db logo" className="logo" />
+        </Link>
+
+        <ul className='nav-links'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li to='/about'>
+            <Link>About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
-
-export default Navbar
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const About = () => {
+export default function About() {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About US</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium id dolorem aut harum! Voluptas libero impedit ullam quas aliquam ipsam perspiciatis et perferendis recusandae excepturi. Earum quisquam, a voluptates fugit numquam inventore ex in? Ut amet dolorum porro impedit! Quas?
+      </p>
+    </section>
   )
 }
-
-export default About
